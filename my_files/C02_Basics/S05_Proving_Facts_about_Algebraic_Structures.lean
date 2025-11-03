72c86eb1f6839e467f894a485b95a13a8ecc0a29
yeaayy <154654039+yeaayy@users.noreply.github.com>
2025-11-04 02:22:46 +0700

    Update

--- my_files/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ my_files/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -36,22 +36,22 @@ variable (x y z : α)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
 example : x ⊓ y = y ⊓ x := by
-  sorry
+  apply inf_comm
 
 example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
-  sorry
+  apply inf_assoc
 
 example : x ⊔ y = y ⊔ x := by
-  sorry
+  apply sup_comm
 
 example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
-  sorry
+  apply sup_assoc
 
 theorem absorb1 : x ⊓ (x ⊔ y) = x := by
-  sorry
+  apply le_antisymm inf_le_left (le_inf (le_refl x) le_sup_left)
 
 theorem absorb2 : x ⊔ x ⊓ y = x := by
-  sorry
+  apply le_antisymm (sup_le (le_refl x) inf_le_left) le_sup_left
 
 end
 
@@ -70,10 +70,18 @@ variable {α : Type*} [Lattice α]
 variable (a b c : α)
 
 example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b ⊓ c = (a ⊔ b) ⊓ (a ⊔ c) := by
-  sorry
+  calc
+    a ⊔ b ⊓ c = a ⊔ a ⊓ c ⊔ b ⊓ c         := by rw [sup_inf_self]
+    _         = a ⊔ (a ⊔ b) ⊓ c           := by rw [inf_comm a c, inf_comm b c, sup_assoc, ← h, inf_comm]
+    _         = (a ⊔ b) ⊓ a ⊔ (a ⊔ b) ⊓ c := by rw [← inf_comm a, inf_sup_self]
+    _         = (a ⊔ b) ⊓ (a ⊔ c)         := by rw [← h]
 
 example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c := by
-  sorry
+  calc
+    a ⊓ (b ⊔ c) = a ⊓ (a ⊔ c) ⊓ (b ⊔ c)     := by rw [inf_sup_self]
+    _           = a ⊓ (a ⊓ b ⊔ c)           := by rw [sup_comm a c, sup_comm b c, inf_assoc, ← h, ← sup_comm]
+    _           = (a ⊓ b ⊔ a) ⊓ (a ⊓ b ⊔ c) := by rw [← sup_comm a, sup_inf_self]
+    _           = a ⊓ b ⊔ a ⊓ c             := by rw [← h]
 
 end
 
@@ -87,13 +95,24 @@ variable (a b c : R)
 #check (mul_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a * b)
 
 example (h : a ≤ b) : 0 ≤ b - a := by
-  sorry
+  have h₁ := add_le_add_right h (-a)
+  rw [add_neg_cancel, ← sub_eq_add_neg] at h₁
+  exact h₁
 
 example (h: 0 ≤ b - a) : a ≤ b := by
-  sorry
+  have h₁ := add_le_add_right h a
+  rw [zero_add, sub_eq_add_neg, add_assoc, neg_add_cancel, add_zero] at h₁
+  exact h₁
 
 example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := by
-  sorry
+  rw [← add_zero (a * c)]
+  apply add_le_of_le_sub_left
+  rw [← mul_sub_right_distrib]
+  apply mul_nonneg
+  . apply le_sub_right_of_add_le
+    rw [zero_add]
+    exact h
+  . exact h'
 
 end
 
@@ -106,7 +125,10 @@ variable (x y z : X)
 #check (dist_triangle x y z : dist x z ≤ dist x y + dist y z)
 
 example (x y : X) : 0 ≤ dist x y := by
-  sorry
+  have h : 0 ≤ dist x y * 2 := by
+    rw [mul_two, ← dist_self x]
+    nth_rewrite 3 [dist_comm]
+    exact dist_triangle x y x
+  apply nonneg_of_mul_nonneg_left h zero_lt_two
 
 end
-
