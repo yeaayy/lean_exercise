53f5749f44c2dc1fb39805087075d518a48f6073
yeaayy <154654039+yeaayy@users.noreply.github.com>
2025-11-03 22:04:37 +0700

    Update

--- my_files/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ my_files/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,59 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply ge_antisymm
+  repeat
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+
+  apply le_min
+  . apply min_le_of_left_le
+    apply min_le_left
+  . apply min_le_min
+    apply min_le_right
+    rfl
+
+  apply le_min
+  . apply min_le_min
+    rfl
+    apply min_le_left
+  . apply min_le_of_right_le
+    apply min_le_right
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  . apply add_le_add_right
+    apply min_le_left
+  . apply add_le_add_right
+    apply min_le_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  apply aux
+  rw [← add_neg_cancel_right (min (a + c) (b + c)) c, add_assoc, add_comm c, ← add_assoc]
+  apply add_le_add_right
+  apply le_min
+  . apply le_of_add_le_add_right
+    rw [add_neg_cancel_right, neg_neg]
+    apply min_le_left
+  . apply le_of_add_le_add_right
+    rw [add_neg_cancel_right, neg_neg]
+    apply min_le_right
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+#check add_sub_cancel_right
+#check sub_add_cancel
+
+example : |a| - |b| ≤ |a - b| := by
+  nth_rewrite 1 [← sub_add_cancel a b]
+  apply le_trans ∘ sub_le_iff_le_add.mpr
+  apply abs_add
+  rfl
 end
 
 section
@@ -66,7 +108,11 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  repeat apply dvd_add
+  rw [mul_comm x z, ← mul_assoc]
+  repeat apply dvd_mul_left
+  apply dvd_trans h
+  apply dvd_mul_left
 end
 
 section
@@ -78,7 +124,5 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply Nat.gcd_comm
 end
-
-
