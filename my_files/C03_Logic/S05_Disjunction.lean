e0d767eaa0494f63c68764464ae8ee8e810fd5ee
yeaayy <154654039+yeaayy@users.noreply.github.com>
2025-11-05 00:43:56 +0700

    Update

--- my_files/C03_Logic/S05_Disjunction.lean
+++ my_files/C03_Logic/S05_Disjunction.lean
@@ -58,19 +58,39 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  rcases abs_cases x with ⟨h1,h2⟩ | ⟨h1,h2⟩
+  . rw [h1]
+  . linarith
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rcases abs_cases x with ⟨h1,h2⟩ | ⟨h1,h2⟩ <;> linarith
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases abs_cases x with ⟨h1,h2⟩ | ⟨h1,h2⟩ <;>
+  rcases abs_cases y with ⟨h3,h4⟩ | ⟨h3,h4⟩ <;>
+  rcases abs_cases (x + y) with ⟨h5,h6⟩ | ⟨h5,h6⟩ <;>
+  linarith
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  constructor
+  . intro h
+    rcases abs_cases x with ⟨h1,h2⟩ | ⟨h1,h2⟩ <;>
+    . rcases abs_cases y with ⟨h3,h4⟩ | ⟨h3,h4⟩
+      left
+      linarith
+      right
+      linarith
+  . intro h
+    rcases h with h | h <;>
+    rcases abs_cases y with ⟨h1,h2⟩ | ⟨h1,h2⟩ <;>
+    linarith
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  constructor
+  . intro h
+    rcases abs_cases x with ⟨h1,h2⟩ | ⟨h1,h2⟩ <;> constructor <;> linarith
+  . intro ⟨h1, h2⟩
+    rcases abs_cases x with ⟨h1,h2⟩ | ⟨h1,h2⟩ <;> linarith
 
 end MyAbs
 
@@ -91,23 +111,70 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨a, b, h1, h2⟩ <;> linarith [pow_two_nonneg a, pow_two_nonneg b]
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have : (x + 1) * (x - 1) = 0 := by
+    calc
+      (x + 1) * (x - 1)
+      _ = x * x - 1     := by linarith
+      _ = 0             := by
+        rw [← pow_two, sub_eq_zero]
+        exact h
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero this with h | h
+  right
+  linarith
+  left
+  linarith
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have : (x + y) * (x - y) = 0 := by
+    calc
+      (x + y) * (x - y)
+      _ = x * x - y * y := by linarith
+      _ = 0 := by
+        rw [← pow_two, ← pow_two, sub_eq_zero]
+        exact h
+
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero this with h | h
+  right
+  linarith
+  left
+  linarith
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have : (x + 1) * (x - 1) = 0 := by
+    calc
+      (x + 1) * (x - 1)
+      _ = x * x - 1     := by ring
+      _ = 0             := by
+        rw [← pow_two, sub_eq_zero]
+        assumption
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero this with h | h
+  right
+  apply add_eq_zero_iff_eq_neg.mp at h
+  assumption
+  left
+  rw [sub_eq_zero] at h
+  assumption
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have := by calc
+    (x + y) * (x - y)
+    _ = x * x - y * y := by ring
+    _ = 0 := by
+      rw [pow_two, pow_two, ← sub_eq_zero] at h
+      assumption
+
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero this with h | h
+  . right
+    exact add_eq_zero_iff_eq_neg.mp h
+  . left
+    exact sub_eq_zero.mp h
 
 end
 
@@ -124,5 +191,14 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+  constructor
+  . intro h
+    by_cases p: P
+    . right
+      exact h p
+    . left
+      assumption
+  . intro h p
+    cases h
+    . contradiction
+    . assumption
