1f6041abf8baef193571811ff337f37d761eb500
yeaayy <154654039+yeaayy@users.noreply.github.com>
2025-12-28 03:38:23 +0700

    Finish C05 S04

--- my_files/C05_Elementary_Number_Theory/S04_More_Induction.lean
+++ my_files/C05_Elementary_Number_Theory/S04_More_Induction.lean
@@ -112,7 +112,11 @@ theorem fib_add' : ∀ m n, fib (m + n + 1) = fib m * fib n + fib (m + 1) * fib 
     simp only [fib_add_two, Nat.succ_eq_add_one, this]
     ring
 
-example (n : ℕ): (fib n) ^ 2 + (fib (n + 1)) ^ 2 = fib (2 * n + 1) := by sorry
+example (n : ℕ): (fib n) ^ 2 + (fib (n + 1)) ^ 2 = fib (2 * n + 1) := by
+  convert (fib_add n n).symm using 1
+  linarith
+  rw [← two_mul n]
+
 example (n : ℕ): (fib n) ^ 2 + (fib (n + 1)) ^ 2 = fib (2 * n + 1) := by
   rw [two_mul, fib_add, pow_two, pow_two]
 
@@ -142,4 +146,3 @@ theorem zero_lt_of_mul_eq_one (m n : ℕ) : n * m = 1 → 0 < n ∧ 0 < m := by
 example (m n : ℕ) : n*m = 1 → 0 < n ∧ 0 < m := by
   rcases m with (_ | m); simp
   rcases n with (_ | n) <;> simp
-
