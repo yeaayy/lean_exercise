8a7da5cc3e7584e5e27af9af5767a5d55629fbd0
yeaayy <154654039+yeaayy@users.noreply.github.com>
2026-03-14 14:46:36 +0700

    Finish C07 S02

--- my_files/C07_Structures/S02_Algebraic_Structures.lean
+++ my_files/C07_Structures/S02_Algebraic_Structures.lean
@@ -54,8 +54,14 @@ def permGroup {α : Type*} : Group₁ (Equiv.Perm α)
   inv_mul_cancel := Equiv.self_trans_symm
 
 structure AddGroup₁ (α : Type*) where
-  (add : α → α → α)
-  -- fill in the rest
+  add : α → α → α
+  zero : α
+  neg : α → α
+  add_assoc : ∀ x y z : α, add (add x y) z = add x (add y z)
+  add_zero : ∀ x : α, add x zero = x
+  zero_add : ∀ x : α, add zero x = x
+  neg_add_cancel : ∀ x : α, add (neg x) x = zero
+
 @[ext]
 structure Point where
   x : ℝ
@@ -67,11 +73,18 @@ namespace Point
 def add (a b : Point) : Point :=
   ⟨a.x + b.x, a.y + b.y, a.z + b.z⟩
 
-def neg (a : Point) : Point := sorry
+def neg (a : Point) : Point := ⟨-a.x, -a.y, -a.z⟩
 
-def zero : Point := sorry
+def zero : Point := ⟨0, 0, 0⟩
 
-def addGroupPoint : AddGroup₁ Point := sorry
+def addGroupPoint : AddGroup₁ Point where
+  add := add
+  zero := zero
+  neg := neg
+  add_assoc := by unfold add; simp [add_assoc]
+  add_zero := by unfold add zero; simp
+  zero_add := by unfold add zero; simp
+  neg_add_cancel := by unfold neg add zero; simp
 
 end Point
 
@@ -170,4 +183,20 @@ end
 
 class AddGroup₂ (α : Type*) where
   add : α → α → α
-  -- fill in the rest
+  zero : α
+  neg : α → α
+  add_assoc : ∀ x y z : α, add (add x y) z = add x (add y z)
+  add_zero : ∀ x : α, add x zero = x
+  zero_add : ∀ x : α, add zero x = x
+  neg_add_cancel : ∀ x : α, add (neg x) x = zero
+
+instance : AddGroup₂ Point where
+  add := Point.add
+  zero := Point.zero
+  neg := Point.neg
+  add_assoc := by unfold Point.add; simp [add_assoc]
+  add_zero := by unfold Point.add Point.zero; simp
+  zero_add := by unfold Point.add Point.zero; simp
+  neg_add_cancel := by unfold Point.neg Point.add Point.zero; simp
+
+example {a b : Point} : a + b = a.add b := rfl
